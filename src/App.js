50646e0ed45fd2e9ca974aadba45a07b921e3a6f
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-26 21:36:13 -0600

    Couldn't get the search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList.js';
 import  WelcomePage from './components/WelcomePage.js';
 import { Route } from 'react-router-dom';
