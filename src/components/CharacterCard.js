66f9f499f5e532078363b96d2fed2bfad68dbf6e
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-26 19:22:32 -0600

    fetched a list of characters and have them displaying on the screen with some info

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard({characters}) {
+  console.log(characters)
+    return( 
+      <div className="characters">
+        <img src={characters.image} alt={characters.name} />
+        <h1>{characters.name}</h1>
+        <h3>Species:{characters.species} Status: {characters.status}</h3>
+        <h3>Location: {characters.location.name} Origin:{characters.origin.name}</h3>
+        {console.log(characters)}
+      </div>
+  );    
 }
+
+export default CharacterCard;
